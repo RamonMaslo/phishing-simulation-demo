88f6ba19dc3145c8ea8c79f74038d45f123078d4
Ramon <ramon.wedowski@gdata.de>
2022-08-29 08:30:21 +0200

    :tada: First commit

---
+++ src/main/java/com/gdata/phishingsimulationdemo/PhishingSimulationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gdata.phishingsimulationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishingSimulationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhishingSimulationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/groovy/com/gdata/phishingsimulationdemo/SmokeSpec.groovy
@@ -0,0 +1,10 @@
+package com.gdata.phishingsimulationdemo
+
+import spock.lang.Specification
+
+class SmokeSpec extends Specification {
+
+    def "test" () {
+        expect: true
+    }
+}
